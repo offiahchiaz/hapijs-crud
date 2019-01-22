af4d802f1188135282aa7dfc416c408e02b0d591
Offiahchiaz <offiahchiaz@gmail.com>
2019-01-22 12:34:35 +0100

    Add static files

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
 // Add Connection
 const server = Hapi.server({
     port: 4000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public') 
+        }
+    }
 });
 
 server.route({
@@ -16,7 +22,25 @@ server.route({
     }
 });
 
+server.route({
+    method: 'GET',
+    path: '/users/{name}',
+    handler: (request, h) => {
+        return  `${request.params.name}, you are welcome`
+    }
+});
+
 const init = async () => {
+
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return h.file('about.html');
+        }
+    });
 
     await server.start();
     console.log(`Server running on port ${server.info.uri}`);
